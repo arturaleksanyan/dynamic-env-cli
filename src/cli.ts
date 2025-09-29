e5c0dc4e445b9520dbbda5e5fdc5026cb2af1051
Artur Aleksanyan <a.aleksanyan@inorain.com>
2025-09-29 12:04:17 +0400

    Update project configuration: increment version to 1.0.10, enhance package.json with description, repository, and bugs information. Add .gitignore entries for dist and node_modules. Remove deprecated test.sh script and monitoring registration logic from cli.ts.

--- src/cli.ts
+++ src/cli.ts
@@ -29,32 +29,6 @@ done
 rm -f "$TEMP_FILE"
 
 
-#Register with monitoring service
-if [ -n "$APP_NAME" ] && [ -n "$SERVICE_NAME" ] && [ -n "$HOST" ] && [ -n "$MONITORING_HOST" ]; then
-  CONF=$(cat <<EOF
-{
-  "app_name": "$APP_NAME",
-  "service_name": "$SERVICE_NAME",
-  "host": "$HOST"
-}
-EOF
-)
-
-  RESPONSE=$(curl -s -w "
-HTTP_CODE:%{http_code}" -X POST "$MONITORING_HOST/api/register"     -H "Content-Type: application/json"     -d "$CONF")
-
-  HTTP_BODY=$(echo "$RESPONSE" | sed '$d')
-  HTTP_CODE=$(echo "$RESPONSE" | tail -n1 | cut -d':' -f2)
-
-  echo "Response Body:"
-  echo "$HTTP_BODY"
-  echo "HTTP Status Code: $HTTP_CODE"
-
-  touch "healthchecks"
-else
-  echo "Skipping monitoring registration: missing required env vars."
-fi
-
 # Handle arguments
 for arg in "$@"; do
   case $arg in
